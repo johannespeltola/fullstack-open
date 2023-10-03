447f831415dc263f41f59db111494589e4411995
Johannes Peltola <johannes.peltola@codebite.fi>
2023-10-03 17:56:07 +0300

    2.19

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,4 +1,27 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+const CountryDetails = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>Capital {country.capital[0]}</p>
+      <p>Area {country.area}</p>
+      <h2>Languages:</h2>
+      <ul>
+        {Object.values(country.languages).map((e) => <li key={e}>{e}</li>)}
+      </ul>
+      <img src={country.flags.png} alt='flag' />
+    </div>
+  )
+}
+
 const Countries = ({ countries }) => {
+  const [expanded, setExpanded] = useState({})
+
+  useEffect(() => {
+    setExpanded({})
+  }, [countries])
   if (!countries?.length) {
     return <p>No results, specify another filter</p>
   }
@@ -7,22 +30,23 @@ const Countries = ({ countries }) => {
   }
   if (countries.length === 1) {
     return (
-      <div>
-        <h1>{countries[0].name.common}</h1>
-        <p>Capital {countries[0].capital[0]}</p>
-        <p>Area {countries[0].area}</p>
-        <h2>Languages:</h2>
-        <ul>
-          {Object.values(countries[0].languages).map((e) => <li key={e}>{e}</li>)}
-        </ul>
-        <img src={countries[0].flags.png} alt='flag' />
-      </div>
+      <CountryDetails country={countries[0]} />
     )
   }
   return (
     <div>
-      {countries.map((c) => <p key={c.name.common}>{c.name.common}</p>)}
-    </div>
+      {countries.map((c) =>
+        <div key={c.name.common}>
+          <p>
+            {c.name.common}
+            <button onClick={() => setExpanded({ ...expanded, [c.name.common]: !expanded[c.name.common] })}>
+              {expanded[c.name.common] ? 'hide' : 'show'}
+            </button>
+          </p>
+          {expanded[c.name.common] && <CountryDetails country={c} />}
+        </div>
+      )}
+    </div >
   )
 }
 export default Countries
