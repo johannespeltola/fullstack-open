5694a9430bd1358c895908837297723b26a4bef9
Johannes Peltola <johannes.peltola@codebite.fi>
2023-10-10 15:45:14 +0300

    3.7

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const { getRandomInt } = require('./utils')
 const AppError = require('./error')
 
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {
