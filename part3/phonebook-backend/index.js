ea3bcbd79fa5eac8701f53ec430df3d13d27ff6c
Johannes Peltola <johannes.peltola@codebite.fi>
2023-10-10 20:02:27 +0300

    3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -21,35 +21,17 @@ app.use(morgan((tokens, req, res) => [
 ))
 app.use(express.static('dist'))
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
+app.get('/info', async (req, res, next) => {
+  try {
+    const persons = await Person.find()
+    res.setHeader('Content-Type', 'text/html')
+    res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `)
+  } catch (error) {
+    next(error)
   }
-]
-
-app.get('/info', (req, res) => {
-  res.setHeader('Content-Type', 'text/html')
-  res.send(`
-  <p>Phonebook has info for ${persons.length} people</p>
-  <p>${new Date()}</p>
-  `)
 })
 
 app.get('/api/persons', async (req, res, next) => {
