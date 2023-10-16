b1abbfa51466fd90656c0be7d86fc8207e5d2dc1
Johannes Peltola <johannes.peltola@codebite.fi>
2023-10-16 15:49:58 +0300

    4.5

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
@@ -6,7 +7,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((prev, curr) => prev + curr.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce((prev, current) => (prev?.likes > current.likes) ? prev : current, null)
+  return favorite
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
