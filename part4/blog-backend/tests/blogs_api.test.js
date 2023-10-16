d3f174bb73fd34142286b5da4f72b1b12afdcdd1
Johannes Peltola <johannes.peltola@codebite.fi>
2023-10-16 18:08:53 +0300

    4.10

--- part4/blog-backend/tests/blogs_api.test.js
+++ part4/blog-backend/tests/blogs_api.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
 const { listWithManyBlogs } = require('./data')
+const { blogsInDb } = require('./test_helper')
 
 const api = supertest(app)
 
@@ -24,6 +25,21 @@ test('blogs are returned as json', async () => {
 test('blogs are have id property defined', async () => {
   const res = await api.get('/api/blogs')
   res.body.forEach((e) => expect(e.id).toBeDefined())
+})
+
+test('blog is successfully created', async () => {
+  const newBlog = {
+    title: 'Interesting Blog Title',
+    author: 'John Doe',
+    url: 'google.com'
+  }
+  const blogsBefore = await blogsInDb(newBlog)
+  const res = await api.post('/api/blogs').send(newBlog)
+  expect(res.status).toBe(201)
+  const blogsAfter = await blogsInDb(newBlog)
+  expect(blogsAfter).toHaveLength(blogsBefore.length + 1)
+  const blogIds = blogsAfter.map((e) => e.id)
+  expect(blogIds).toContain(res.body.id)
 })
 
 afterAll(async () => {
