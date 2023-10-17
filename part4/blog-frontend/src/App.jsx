7135444b12d5f1808bc17a9dd17030b32f8edae0
Johannes Peltola <johannes.peltola@codebite.fi>
2023-10-17 16:44:45 +0300

    5.3 - 5.4

--- part4/blog-frontend/src/App.jsx
+++ part4/blog-frontend/src/App.jsx
@@ -4,31 +4,48 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import Login from './components/Login'
 import Notification from './components/Notification'
+import CreateBlog from './components/CreateBlog'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState('')
+  const [severity, setSeverity] = useState('error')
+
+  const notification = (severity, message) => {
+    setSeverity(severity)
+    setErrorMessage(message)
+    setTimeout(() => { setErrorMessage(null) }, 5000)
+  }
+
+  const fetchBlogs = async () => {
+    try {
+      setBlogs(await blogService.getAll())
+    } catch (error) {
+      notification('error', 'Failed to retrieve blogs')
+    }
+  }
 
   useEffect(() => {
     const storedUser = localStorage.getItem('user')
     if (storedUser) {
-      setUser(JSON.parse(storedUser))
+      const user = JSON.parse(storedUser)
+      setUser(user)
+      blogService.setToken(user.token)
     }
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
+    fetchBlogs()
   }, [])
 
   const handleLogin = async (username, password) => {
     try {
       const user = await loginService.login(username, password)
       setUser(user)
+      blogService.setToken(user.token)
       localStorage.setItem('user', JSON.stringify(user))
+      notification('success', `Welcome back ${user.name}!`)
       return true
     } catch (error) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => { setErrorMessage(null) }, 5000)
+      notification('error', error.response.data.error)
     }
     return false
   }
@@ -36,21 +53,35 @@ const App = () => {
   const handleLogout = async () => {
     setUser(null)
     localStorage.removeItem('user')
+    notification('success', 'Successfully logged out')
+  }
+
+  const handleBlogSubmit = async ({ title, author, url }) => {
+    try {
+      const blog = await blogService.create({ title, author, url })
+      setBlogs([...blogs, blog])
+      notification('success', `Added new blog ${title} by ${author}`)
+      return true
+    } catch (error) {
+      notification('error', error.response.data.error)
+    }
+    return false
   }
 
   return (
     <div>
-      <Notification message={errorMessage} />
-      {user ? (
+      <Notification message={errorMessage} severity={severity} />
+      {!user ? <Login handleLogin={handleLogin} /> : (
         <>
           <h2>blogs</h2>
           <p>{user.name} logged in <button onClick={handleLogout}>Logout</button></p>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
+          <h2>Create new</h2>
+          <CreateBlog submit={handleBlogSubmit} />
         </>
-      ) : <Login handleLogin={handleLogin} />
-      }
+      )}
     </div>
   )
 }
--- part4/blog-frontend/src/services/blogs.js
+++ part4/blog-frontend/src/services/blogs.js
@@ -1,9 +1,25 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+let token = null
+const setToken = newToken => { token = `Bearer ${newToken}` }
+
+const getAll = async () => {
+  const res = await axios.get(baseUrl)
+  return res.data
 }
 
-export default { getAll }
+const create = async ({ title, author, url }) => {
+  const res = await axios.post(
+    baseUrl,
+    { title, author, url },
+    { headers: { Authorization: token } }
+  )
+  return res.data
+}
+
+export default {
+  getAll,
+  create,
+  setToken
+}
