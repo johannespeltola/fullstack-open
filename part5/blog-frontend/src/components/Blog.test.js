036b1dfec87cefc407744e19760a6215e8b362e4
Johannes Peltola <johannes.peltola@codebite.fi>
2023-12-17 18:49:21 +0200

    5.14

--- part5/blog-frontend/src/components/Blog.test.js
+++ part5/blog-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('default renders only title and author', () => {
@@ -20,4 +21,29 @@ test('default renders only title and author', () => {
   expect(url).toBeNull()
   const likes = screen.queryByText(blog.likes)
   expect(likes).toBeNull()
+})
+
+test('url and likes shown when expanded', async () => {
+  const blog = {
+    title: 'Title',
+    author: 'Mr. Author',
+    url: 'google.com',
+    likes: 12,
+    user: '657f06d10cd7c31ce3e6d826'
+  }
+
+  const user = userEvent.setup()
+
+  render(<Blog blog={blog} />)
+
+  const button = screen.getByText('View')
+
+  await user.click(button)
+
+  const title = screen.getByText(`${blog.title} ${blog.author}`)
+  expect(title).toBeDefined()
+  const url = screen.getByText(blog.url)
+  expect(url).toBeDefined()
+  const likes = screen.getByText(`Likes ${blog.likes}`)
+  expect(likes).toBeDefined()
 })
