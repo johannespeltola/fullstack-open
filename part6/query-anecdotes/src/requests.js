1c6428be10c9f8ffab00cde6d42b75dd86dc2ebf
Johannes Peltola <johannes.peltola@codebite.fi>
2023-12-19 20:25:17 +0200

    6.21

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -1,3 +1,9 @@
 import axios from 'axios'
 
-export const getAnecdotes = () => axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+export const getAnecdotes = () =>
+  axios.get('http://localhost:3001/anecdotes')
+    .then((res) => res.data)
+
+export const createAnecdote = (anecdote) =>
+  axios.post('http://localhost:3001/anecdotes', anecdote)
+    .then((res) => res.data)
